22871823c1ce52c92a837716230bb7b76104540f
Werozel <khaibulaev.mn@gmail.com>
2022-04-14 21:31:52 +0300

    Fixed static files
    Provided a path for static files to app

--- modules/core/app.py
+++ modules/core/app.py
@@ -4,7 +4,7 @@ from flask_bootstrap import Bootstrap
 
 from config import SECRET_KEY
 
-app = Flask(__name__, template_folder="../../templates")
+app = Flask(__name__, template_folder="../../templates", static_folder="../../static")
 app.config['SECRET_KEY'] = SECRET_KEY
 
 bootstrap = Bootstrap(app)
