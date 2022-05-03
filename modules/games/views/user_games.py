693995b60c62901357ca01331e8332f0e6c7e717
Werozel <khaibulaev.mn@gmail.com>
2022-05-03 22:10:05 +0300

    Sorted my games
    Sort games by their statuses

--- modules/games/views/user_games.py
+++ modules/games/views/user_games.py
@@ -2,6 +2,7 @@ from flask import render_template
 from flask_login import current_user, login_required
 
 from modules.core.app import app
+from modules.games.models.games import GameStatus
 from modules.users.models.user import User
 
 
@@ -10,4 +11,28 @@ from modules.users.models.user import User
 def render_my_games():
     user: User = current_user
     all_games = user.games
-    return render_template("my_games.html", games=all_games)
+    pending_games = list(
+        filter(
+            lambda x: x.status == GameStatus.PENDING,
+            all_games
+        )
+    )
+    progress_games = list(
+        filter(
+            lambda x: x.status == GameStatus.IN_PROGRESS,
+            all_games
+        )
+    )
+    completed_games = list(
+        filter(
+            lambda x: x.status == GameStatus.COMPLETED,
+            all_games
+        )
+    )
+
+    return render_template(
+        "my_games.html",
+        pending_games=pending_games,
+        progress_games=progress_games,
+        completed_games=completed_games
+    )
