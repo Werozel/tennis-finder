fd787b8fb20ba2119356df71657a9a7bcdc9885d
Werozel <khaibulaev.mn@gmail.com>
2022-05-11 22:51:32 +0300

    Added all translations
    Added all the translations for each string

--- helpers/translate.py
+++ helpers/translate.py
@@ -5,19 +5,12 @@ from shutil import move, copymode
 from os import fdopen, remove
 import sys
 
-translator = Translator()
+from httpcore import ReadTimeout
 
 src_lang = "en"
 dest_lang = "ru"
 
 file_path = 'tmp.po'
-
-print("started")
-# Create temp file
-fh, abs_path = mkstemp()
-new_file = fdopen(fh, 'w')
-old_file = open(file_path)
-
 is_windows = sys.platform.startswith('win32')
 
 
@@ -28,30 +21,42 @@ def write_new(text: str):
     new_file.write(text)
 
 
-line_to_translate = None
-for line in old_file:
-    if line.startswith('msgid'):
-        line_to_translate = line[7:-2]
-        write_new(line)
-    elif line.startswith('msgstr'):
-        current_line = line[8:-2]
-        if len(current_line) > 0 or line_to_translate is None or len(line_to_translate) == 0:
+if __name__ == "__main__":
+    translator = Translator()
+
+    print("started")
+    # Create temp file
+    fh, abs_path = mkstemp()
+    new_file = fdopen(fh, 'w')
+    old_file = open(file_path)
+
+    line_to_translate = None
+    for line in old_file:
+        if line.startswith('msgid'):
+            line_to_translate = line[7:-2]
             write_new(line)
-            continue
-        translated_line = translator.translate(line_to_translate, src=src_lang, dest=dest_lang).text
-        write_new(f"msgstr \"{translated_line}\"\n")
-        line_to_translate = None
-    else:
-        line_to_translate = None
-        write_new(line)
+        elif line.startswith('msgstr'):
+            current_line = line[8:-2]
+            if len(current_line) > 0 or line_to_translate is None or len(line_to_translate) == 0:
+                write_new(line)
+                continue
+            print(f"translating: {line_to_translate}")
+            try:
+                translated_line = translator.translate(line_to_translate, src=src_lang, dest=dest_lang).text
+            except ReadTimeout:
+                break
+            write_new(f"msgstr \"{translated_line}\"\n")
+            line_to_translate = None
+        else:
+            line_to_translate = None
+            write_new(line)
 
+    old_file.close()
+    new_file.close()
 
-old_file.close()
-new_file.close()
-
-# Copy the file permissions from the old file to the new file
-copymode(file_path, abs_path)
-# Remove original file
-remove(file_path)
-# Move new file
-move(abs_path, file_path)
+    # Copy the file permissions from the old file to the new file
+    copymode(file_path, abs_path)
+    # Remove original file
+    remove(file_path)
+    # Move new file
+    move(abs_path, file_path)
