72ed40810ccadb840715b63a99cce051399b32e1
Cactiw <Knyazev1514@gmail.com>
2022-05-28 13:01:49 +0300

    Fixed create_config script

--- tennis_finder/create_config.py
+++ tennis_finder/create_config.py
@@ -1,3 +1,4 @@
+import os
 import secrets
 import string
 
@@ -6,7 +7,7 @@ def generate_secure_random_string(n: int) -> str:
     return ''.join(secrets.choice(string.ascii_letters + string.digits) for _ in range(n))
 
 
-with open("config.py", "w", encoding="utf-8") as f:
+with open("tennis_finder/config.py", "w", encoding="utf-8") as f:
     f.write("PORT = 5555\n")
     f.write("DB_URL = \"postgresql+psycopg2://dbuser:dbpass@localhost:5432/dbpass\"\n")
     f.write(f"SECRET_KEY = \"{generate_secure_random_string(32)}\"\n")
