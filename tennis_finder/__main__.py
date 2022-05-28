c9a4b63a31169e29c032962c8ee256c56d3aed72
Cactiw <Knyazev1514@gmail.com>
2022-05-28 14:19:57 +0300

    Added config create script

--- tennis_finder/__main__.py
+++ tennis_finder/__main__.py
@@ -1,6 +1,12 @@
 """Run the tennis_finder package."""
+import os
+
 from tennis_finder.main import run_app
 
 
 if __name__ == "__main__":
+    if "config.py" not in os.listdir("tennis_finder"):
+        import tennis_finder.create_config as cf
+        cf.generate_secure_random_string(10)
+
     run_app()
--- tennis_finder/create_config.py
+++ tennis_finder/create_config.py
@@ -9,7 +9,7 @@ def generate_secure_random_string(n: int) -> str:
 
 with open("tennis_finder/config.py", "w", encoding="utf-8") as f:
     f.write("PORT = 5555\n")
-    f.write("DB_URL = \"postgresql+psycopg2://dbuser:dbpass@localhost:5432/dbpass\"\n")
+    f.write("DB_URL = \"postgresql+psycopg2://tennis:tennisPass@localhost:5432/tennis\"\n")
     f.write(f"SECRET_KEY = \"{generate_secure_random_string(32)}\"\n")
     f.write(f"salt = \"{generate_secure_random_string(32)}\"\n")
     f.write(f"salt1 = \"{generate_secure_random_string(32)}\"\n")
